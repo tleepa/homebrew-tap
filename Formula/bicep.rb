55e63f9fa1ff5cfc8a060eeb518dd8b2e0958a90
tleepa <7734919+tleepa@users.noreply.github.com>
2026-06-10 07:05:49 +0000

    Update bicep to v0.44.1

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,7 +1,7 @@
 class Bicep < Formula
   desc "Declarative language for describing and deploying Azure resources"
   homepage "https://github.com/Azure/bicep"
-  version "0.43.8"
+  version "0.44.1"
   license "MIT"
 
   livecheck do
@@ -11,25 +11,25 @@ class Bicep < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/Azure/bicep/releases/download/v0.43.8/bicep-osx-x64"
-      sha256 "e009bd793763266878f39dd05212d2b4a7ed0714d911b7a1e89816e40d05a39d"
+      url "https://github.com/Azure/bicep/releases/download/v0.44.1/bicep-osx-x64"
+      sha256 "9d3c8f412a82670b2d89126eda5210ce1290a5e3790f373094ea98c813cd90a9"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/Azure/bicep/releases/download/v0.43.8/bicep-osx-arm64"
-      sha256 "1bcbea8523b0437d85504c081d742877c2e9793305225bddf53c1aea537191f7"
+      url "https://github.com/Azure/bicep/releases/download/v0.44.1/bicep-osx-arm64"
+      sha256 "d96a185cd7ce6a685a9d43130ff2298597603d7be9f782c7bef5171ed6884795"
     end
   end
 
   if OS.linux?
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Azure/bicep/releases/download/v0.43.8/bicep-linux-x64"
-      sha256 "03b902a8d4da2df36348d20f039a7e6268798781cdeceb38d8c1760f2843bec8"
+      url "https://github.com/Azure/bicep/releases/download/v0.44.1/bicep-linux-x64"
+      sha256 "e17dc9a9888184886bb0c0051a3230b83b19f342749999f707bc571c3dfd2f45"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Azure/bicep/releases/download/v0.43.8/bicep-linux-arm64"
-      sha256 "1eb4e2b02e2aa5e18d711341b1228509d2319956aa051fa04919fa606e480ddb"
+      url "https://github.com/Azure/bicep/releases/download/v0.44.1/bicep-linux-arm64"
+      sha256 "a9c73b96975a17e49b5dd66725c8dc2451501c77b194156d938719ee4990318b"
     end
   end
 
